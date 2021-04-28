20f1d4efbaf5423ed66455e73b7a25ed7547ff29
playfulRabbit <81336274+G-C415@users.noreply.github.com>
2021-04-28 17:22:54 -0400

    Update index.js

--- md5sum/index.js
+++ md5sum/index.js
@@ -1,6 +1,3 @@
-// Note that the character class includes all letters (case-insensitive!), numbers, spaces, periods, and dashes
-// Also, the exclamation point at the beginning negates the match, so this if statement is for failure cases
-
 // import exec module from child_process to run shell command
 // spawn was recommended for long output, but md5sum is short, so exec is ok
 const { exec } = require("child_process");
@@ -9,7 +6,6 @@ exports.handler = (event, handler, callback) => {
 
     //var input = "test"
     var input = event.queryStringParameters.input.trim();
-    
 
     // command to compute md5 of provided input
     // md5sum requires stdin, so this echos and pipes the value to md5sum
@@ -19,17 +15,15 @@ exports.handler = (event, handler, callback) => {
 
     // execute the full command
     exec(cmd, (error, stdout, stderr) => {
-        
-        // there was an error, so return the error message
-value = "teststring"
 
-if (!/^[a-z0-9 .-]*$/i.test(value)) {
-        console.log('Bad input for ' + input);
-        callback(null, {
+        // there was an error, so return the error message
+        if (error) {
+            callback(null, {
                 statusCode: 400,
-                body: "Please provide only letters, numbers, periods, dashes, and spaces"
-        });
-}
+                body: error
+            });
+        }
+
         // no lambda error, but the command failed, so return the error message
         if (stderr) {
             callback(null, {
@@ -37,6 +31,15 @@ if (!/^[a-z0-9 .-]*$/i.test(value)) {
                 body: stderr
             });
         }
+
+        // no errors, so return the md5 hash
+        callback(null, {
+            statusCode: 200,
+            body: stdout.trim()
+        });
+
+    });
+};
 
         // no errors, so return the md5 hash
         callback(null, {
