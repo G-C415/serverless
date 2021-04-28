a7481c320b87408af0439089ce8cbe6f75c992a6
playfulRabbit <81336274+G-C415@users.noreply.github.com>
2021-04-28 17:02:34 -0400

    Create index.js

--- md5sum/index.js
+++ md5sum/index.js
@@ -15,11 +15,7 @@ exports.handler = (event, handler, callback) => {
     // added sed to remove the trailing " -" that md5sum adds to output
     // added -n because echo includes a newline, causing md5sum to be wrong
     var cmd = 'echo -n "' + input + '" | md5sum | sed "s/ -//"';
-
-    // execute the full command
-    exec(cmd, (error, stdout, stderr) => {
-
-    value = "teststring"
+value = "teststring"
 
 if (!/^[a-z0-9 .-]*$/i.test(value)) {
         console.log('Bad input for ' + input);
@@ -31,6 +27,9 @@ if (!/^[a-z0-9 .-]*$/i.test(value)) {
 
 // Note that the character class includes all letters (case-insensitive!), numbers, spaces, periods, and dashes
 // Also, the exclamation point at the beginning negates the match, so this if statement is for failure cases
+
+    // execute the full command
+    exec(cmd, (error, stdout, stderr) => {
 
 
         // no lambda error, but the command failed, so return the error message
