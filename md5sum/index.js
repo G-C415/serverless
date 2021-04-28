87ef93c025503fc813b5300965c26f3a234ddd93
playfulRabbit <81336274+G-C415@users.noreply.github.com>
2021-04-28 17:10:11 -0400

    Update index.js

--- md5sum/index.js
+++ md5sum/index.js
@@ -2,9 +2,6 @@
 // spawn was recommended for long output, but md5sum is short, so exec is ok
 const { exec } = require("child_process");
 
-// Note that the character class includes all letters (case-insensitive!), numbers, spaces, periods, and dashes
-// Also, the exclamation point at the beginning negates the match, so this if statement is for failure cases
-
 exports.handler = (event, handler, callback) => {
 
     //var input = "test"
@@ -15,6 +12,9 @@ exports.handler = (event, handler, callback) => {
     // added sed to remove the trailing " -" that md5sum adds to output
     // added -n because echo includes a newline, causing md5sum to be wrong
     var cmd = 'echo -n "' + input + '" | md5sum | sed "s/ -//"';
+
+    // execute the full command
+    exec(cmd, (error, stdout, stderr) => {
 value = "teststring"
 
 if (!/^[a-z0-9 .-]*$/i.test(value)) {
@@ -25,12 +25,13 @@ if (!/^[a-z0-9 .-]*$/i.test(value)) {
         });
 }
 
-// Note that the character class includes all letters (case-insensitive!), numbers, spaces, periods, and dashes
-// Also, the exclamation point at the beginning negates the match, so this if statement is for failure cases
-
-    // execute the full command
-    exec(cmd, (error, stdout, stderr) => {
-
+        // there was an error, so return the error message
+        if (error) {
+            callback(null, {
+                statusCode: 400,
+                body: error
+            });
+        }
 
         // no lambda error, but the command failed, so return the error message
         if (stderr) {
