ff4b91d646092340bcbdb1a20906ddfba470a153
playfulRabbit <81336274+G-C415@users.noreply.github.com>
2021-05-01 14:56:19 -0400

    Update index.js

--- md5sum/index.js
+++ md5sum/index.js
@@ -6,9 +6,9 @@ exports.handler = (event, handler, callback) => {
 
     //var input = "test"
     var input = event.queryStringParameters.input.trim();
-    value = "teststring"
 
-if (!/^[a-z0-9 .-]*$/i.test(value)) {
+
+if (!/^[a-z0-9 .-]*$/i.test(input)) {
         console.log('Bad input for ' + input);
         callback(null, {
                 statusCode: 400,
