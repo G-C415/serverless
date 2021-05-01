037b81466995a38bc3ce75cde36d57f9119146a2
playfulRabbit <81336274+G-C415@users.noreply.github.com>
2021-05-01 15:01:25 -0400

    Update index.js

--- md5sum/index.js
+++ md5sum/index.js
@@ -6,16 +6,6 @@ exports.handler = (event, handler, callback) => {
 
     //var input = "test"
     var input = event.queryStringParameters.input.trim();
-    value = input
-
-if (!/^[a-z0-9 .-]*$/i.test(value)) {
-        console.log('Bad input for ' + input);
-        callback(null, {
-                statusCode: 400,
-                body: "Please provide only letters, numbers, periods, dashes, and spaces"
-        });
-}
-
 
     // command to compute md5 of provided input
     // md5sum requires stdin, so this echos and pipes the value to md5sum
@@ -25,6 +15,16 @@ if (!/^[a-z0-9 .-]*$/i.test(value)) {
 
     // execute the full command
     exec(cmd, (error, stdout, stderr) => {
+
+value = input
+
+if (!/^[a-z0-9 .-]*$/i.test(value)) {
+        console.log('Bad input for ' + input);
+        callback(null, {
+                statusCode: 400,
+                body: "Please provide only letters, numbers, periods, dashes, and spaces"
+        });
+}
 
         // there was an error, so return the error message
         if (error) {
